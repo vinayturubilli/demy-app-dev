c10d5cfee6f0ce4fc9828f74c4da6797dda7a634
vinay-turubilli <v_turubilli@apple.com>
2022-06-30 02:40:58 +0530

    first commit

---
+++ src/main/java/com/demyapp/demyapp/DemyAppApplication.java
@@ -0,0 +1,13 @@
+package com.demyapp.demyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemyAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demyapp/demyapp/DemyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demyapp.demyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
