e10dced8f2a9ce471d538b680d50d8273fc22b33
vinay-turubilli <v_turubilli@apple.com>
2022-07-10 02:25:32 +0530

    Internships tab initial content

--- UI/src/Components/Internships/Internships.js
+++ UI/src/Components/Internships/Internships.js
@@ -1,5 +1,55 @@
+import './internships.css';
+
 export default function Internships() {
    return (
-     <div>Internships</div>
+     <div style={{padding: "10px"}}>
+        <div style={{width: "50%", float:"right"}}>
+            <img alt="Internships" width="100%" height="100%" src="internships.png"></img>
+        </div>
+        <div style={{width: "50%", float:"right"}}>
+           <div className="padding-top">
+            We are selecting 50 engineers. We will train these engineers for 3 months on data structures & algorithms , that is required to get into the top product-based companies like Google, Meta, Amazon, Apple, Microsoft, LinkedIn, Flipkart and more.
+            We call it, Master 50. Our goal is to make coders successful, 50 at a time!
+          </div>
+          <div className="padding-top">
+            Mission: Our mission is to place students/experienced professionals into MAANG Companies.
+          </div>
+          <div className="padding-top">
+            MAANG (FAANG):
+            Top 5 companies of the world are Meta, Apple, Amazon, Netflix and Google.
+          </div>
+          <div className="padding-top">
+            WEEK 1 -  Introduction- Execution of a program, Decimal - Binary conversion, Ranges of Data Types and constraints, Complexity Analysis of Algorithms, Big-O Notation, Time & Space Analysis and Constraints, Importance of constraints
+          </div>
+          <div className="padding-top">
+            WEEK 2 -Bit-Manipulation, Bitwise operators, Bit-masking, Modular Arithmetic, Recursion, Thinking Recursively, Recurrence Relations, Sorting Techniques, Two Pointer Technique
+          </div>
+          <div className="padding-top">
+            WEEK 3 - Binary Search, Applications of Binary Search , Lower Bound & Upper Bound, Finding Frequency, Optimization problems, Hashing, Why hashing?, Hashing Techniques, Collision Resolutions, Inbuilt Libraries
+          </div>
+          <div className="padding-top">
+            WEEK 4 - Maps and Sets, Subarrays and Subsequences, String matching, Sieve of Erastosthenes, Segmented Sieve, Game Theory, Nims Game, Counting Game
+          </div>
+          <div className="padding-top">
+            WEEK 5 -  Prefix and Suffix concepts, Collecting water, Stacks, Balanced Paranthesis, Largest Histogram Area, Queues, Sliding Window Maximum
+          </div>
+          <div className="padding-top">
+              WEEK 6 - Linked Lists, Various Operations on linked lists, LRU Cache, Cloning Linked list with random pointer, Doubly-linked list
+            </div>
+          <div className="padding-top">
+              WEEK 7 - Binary Trees, BT and FBT, Traversals, Various operations on Binary Trees, Binary Search Trees, Insertion, Updating and Deletion
+          </div>
+          <div className="padding-top">
+             WEEK 8 - More Problems on Binary Trees, Iterative Traversals, Least Common Ancestor, Heaps, Quick Select, Running Median, Trie, Introduction and Implementation
+          </div>
+          <div className="padding-top">
+            WEEK 9 - Problems on Tries, Maximum XOR pair, Partitioning of string, 1D Dynamic Programming, Approaching DP problem, Problems on Overlapping subproblems, Problems on Optimal Substructure , Longest Increasing Subsequence
+          </div>
+          <div className="padding-top">
+             WEEK 10 - 2D Dynamic Programming, Compute NCR, Knapsack, Matrix chain multiplication, Graphs, Introduction and Implementation, Dijkstra, Topological sort
+          </div>
+       </div>
+     </div>
+
    );
 }
